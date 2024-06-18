0474f28b5ace6802576e9fc3af8a026e04fbf40f
Elise Ngo <elisetvy@gmail.com>
2024-06-18 16:54:02 -0700

    Get all dog info on card and style

--- src/components/Dog/Dog.tsx
+++ src/components/Dog/Dog.tsx
@@ -6,9 +6,23 @@ interface DogProps {
 
 function Dog({ dog }: DogProps) {
   return (
-    <div>
-      <p>{dog.name}</p>
-      <img src={dog.img}></img>
+    <div className="bg-slate-100 rounded-xl overflow-hidden">
+      <div className="h-[200px] overflow-hidden">
+        <img
+          className="aspect-square object-cover h-full w-full"
+          src={dog.img}
+        ></img>
+      </div>
+      <div className="px-4 py-4">
+        <div className="flex items-center gap-2">
+          <p className="text-xl font-bold">{dog.name}</p>
+          <p className="Lexend text-xs uppercase bg-white px-2 py-1 rounded-xl">
+            {dog.breed}
+          </p>
+        </div>
+        <p className="Lexend mt-2">Age: {dog.age} </p>
+        <p className="Lexend">Location: {dog.zip_code}</p>
+      </div>
     </div>
   );
 }
