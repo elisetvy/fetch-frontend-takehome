2ad0fb8e9319970da31a3daad584743a26577ac9
Elise Ngo <elisetvy@gmail.com>
2024-06-18 12:48:06 -0700

    Import auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Auth from "./components/auth/Auth";
 
 function App() {
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center my-10">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Auth />
+    </div>
+  );
 }
 
 export default App;
