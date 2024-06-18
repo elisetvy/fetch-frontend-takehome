ac591f059cc65c65b2aa55776e01138ea8490822
Elise Ngo <elisetvy@gmail.com>
2024-06-18 13:03:50 -0700

    Set up state and write event handlers

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+
 function Auth() {
-    return (
-        <form className="flex flex-col border-black border">
-            <label>Name:</label>
-            <input className="bg-blue-200 px-2 py-1"></input>
-            <label>Email:</label>
-            <input className="bg-blue-200 px-2 py-1"></input>
-            <button>Submit</button>
-        </form>
-    )
+    const initialUserData = {
+        name: "",
+        email: ""
+      };
+
+  const [userData, setUserData] = useState(initialUserData);
+
+  /** Update state within form data. */
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUserData(f => ({ ...f, [name]: value}))
+  }
+
+  /** Call login function with user data. */
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await login(userData);
+  }
+
+  return (
+    <form className="flex flex-col border-black border">
+      <label>Name:</label>
+      <input className="bg-blue-200 px-2 py-1"></input>
+      <label>Email:</label>
+      <input className="bg-blue-200 px-2 py-1"></input>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default Auth;
