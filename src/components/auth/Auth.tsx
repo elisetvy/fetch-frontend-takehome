9022ddc9554ca2f71d9da2e48e5fddb6e1491b00
Elise Ngo <elisetvy@gmail.com>
2024-06-18 14:13:08 -0700

    Change submit to log in

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -57,7 +57,7 @@ function Auth() {
           onClick={handleSubmit}
           className="bg-blue-600 rounded px-6 py-2 w-fit mt-4 text-white hover:opacity-85"
         >
-          Submit
+          Log In
         </button>
       </form>
     </div>
