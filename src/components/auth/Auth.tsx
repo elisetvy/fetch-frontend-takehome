8af8f985aaf938e1d0453ea861d8029a31bce350
Elise Ngo <elisetvy@gmail.com>
2024-06-18 12:54:00 -0700

    Add styling to form

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,6 +1,12 @@
 function Auth() {
     return (
-        <p>Login</p>
+        <form className="flex flex-col border-black border">
+            <label>Name:</label>
+            <input className="bg-blue-200 px-2 py-1"></input>
+            <label>Email:</label>
+            <input className="bg-blue-200 px-2 py-1"></input>
+            <button>Submit</button>
+        </form>
     )
 }
 
