7e83f11cdf75d0045dda90629d9d1ebd4cdd31a7
Elise Ngo <elisetvy@gmail.com>
2024-06-18 12:56:09 -0700

    Remove underline and add bottom padding

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Auth from "./components/auth/Auth";
 function App() {
   return (
     <div className="flex flex-col items-center my-10">
-      <h1 className="text-3xl font-bold underline">The Pawfect Match</h1>
+      <h1 className="text-3xl font-bold pb-10">The Pawfect Match</h1>
       <Auth />
     </div>
   );
