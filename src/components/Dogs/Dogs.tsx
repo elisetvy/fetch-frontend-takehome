93dd82ad754ccaf80bc9985406e0f97c5dc6206d
Elise Ngo <elisetvy@gmail.com>
2024-06-18 16:01:00 -0700

    Write logic to get dog ids then get dogs from ids

--- src/components/Dogs/Dogs.tsx
+++ src/components/Dogs/Dogs.tsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from "react";
+
+import Api from "../api";
+import { Dog } from "../interfaces";
+
 function Dogs() {
-  return <p className="mt-10 text-3xl">here some dogs</p>;
+  const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getDogs() {
+      try {
+        const resp = await Api.searchDogs();
+        const ids = await resp.resultIds;
+        const gottenDogs = await Api.fetchDogs(ids);
+        setDogs(gottenDogs);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getDogs();
+  }, []);
+
+  return (
+    <div className="mt-10">
+      {loading === false && (
+        <>
+          <p className="mt-10 text-3xl">here some dogs</p>
+          <ul>
+            {dogs.map((dog: Dog) => {
+              return <li>{dog.name}</li>;
+            })}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Dogs;
