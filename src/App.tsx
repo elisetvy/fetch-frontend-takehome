3cb14534817fac1b3d2e0a800c7237d3c5d77c9d
Elise Ngo <elisetvy@gmail.com>
2024-06-18 15:14:11 -0700

    Add link to title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 
 import Auth from "./components/auth/Auth";
 import Dogs from "./components/Dogs/Dogs";
@@ -7,7 +7,9 @@ function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col items-center my-10 mx-10">
-        <h1 className="text-3xl font-bold pb-4">The Pawfect Match</h1>
+        <Link to="/" className="text-3xl font-bold pb-4">
+          The Pawfect Match
+        </Link>
         <p className="Lexend min-w-2/4 text-center">
           The Pawfect Match is here to help dog lovers like you search through a
           database of shelter dogs, making it easy to find your furry new best
