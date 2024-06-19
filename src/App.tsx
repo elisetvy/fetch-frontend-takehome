b187ed522c28fcf38388462e9f943b0f45860c4e
Elise Ngo <elisetvy@gmail.com>
2024-06-19 10:20:42 -0700

    Create logout folder/component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import Auth from "./components/auth/Auth";
 import Dogs from "./components/Dogs/Dogs";
+import LogOut from "./components/LogOut/LogOut";
 
 function App() {
   const [currUser, setCurrUser] = useState(
@@ -12,22 +13,23 @@ function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col items-center my-10 mx-10">
-        <Link to="/" className="text-3xl font-bold pb-4">
+        <Link to="/" className="text-3xl font-bold">
           The Pawfect Match
         </Link>
-        <p className="Lexend min-w-2/4 text-center">
+        <p className="Lexend mt-4 min-w-2/4 text-center">
           The Pawfect Match is here to help dog lovers like you search through a
           database of shelter dogs, making it easy to find your furry new best
           friend!
         </p>
+        {currUser && <LogOut setCurrUser={setCurrUser} />}
         <Routes>
-          {currUser === null ? (
+          {currUser ? (
+            <Route path="/" element={<Dogs />}></Route>
+          ) : (
             <Route
               path="/"
               element={<Auth setCurrUser={setCurrUser} />}
             ></Route>
-          ) : (
-            <Route path="/" element={<Dogs />}></Route>
           )}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
---
+++ src/components/LogOut/LogOut.tsx
@@ -0,0 +1,31 @@
+import { AuthProps } from "../interfaces";
+
+import Api from "../api";
+
+function LogOut({ setCurrUser }: AuthProps) {
+  async function handleClick(e) {
+    e.preventDefault();
+
+    try {
+      const resp = await Api.logout();
+
+      if (resp.ok) {
+        sessionStorage.clear();
+        setCurrUser("");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white"
+    >
+      Log Out
+    </button>
+  );
+}
+
+export default LogOut;
