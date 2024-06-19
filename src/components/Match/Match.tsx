f10f8376f098e54a497b22c4c3e1daf5a99d90e0
Elise Ngo <elisetvy@gmail.com>
2024-06-19 16:07:16 -0700

    Remove unnecessary code

--- src/components/Match/Match.tsx
+++ src/components/Match/Match.tsx
@@ -7,15 +7,10 @@ import Api from "../api";
 function Match() {
   const [match, setMatch] = useState(null);
   const [matched, setMatched] = useState(false);
-  const [error, setError] = useState(false);
   const favorites = sessionStorage.getItem("favorites");
 
   async function handleClick(e) {
     e.preventDefault();
-
-    if (favorites === null) {
-      setError(true);
-    }
 
     try {
       const favoritesArray = JSON.parse(favorites);
@@ -30,23 +25,27 @@ function Match() {
 
   return (
     <div className="flex flex-col items-center">
-      {error === true && (
+      {favorites === null && (
         <h1 className="Lexend mt-4 text-red-500">
-          You haven't favorited any dogs yet!
+          Start favoriting some dogs to find a pawfect match!
         </h1>
       )}
       {matched === true && (
         <div className="flex flex-col items-center">
-            <h1 className="Lexend my-4 text-red-500">{match.name} is yo pawfect match</h1>
+          <h1 className="Lexend my-4 text-red-500">
+            {match.name} is yo pawfect match
+          </h1>
           <Dog dog={match} />
         </div>
       )}
-      <button
-        onClick={handleClick}
-        className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white"
-      >
-        Get Match
-      </button>
+      {favorites !== null && (
+        <button
+          onClick={handleClick}
+          className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white"
+        >
+          Get Match
+        </button>
+      )}
     </div>
   );
 }
