afe4e1fbec9d84ccb8700395d3bf9e004d53c26a
Elise Ngo <elisetvy@gmail.com>
2024-06-20 09:24:21 -0700

    Change bg colors

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="grid grid-cols-3">
-        <div className="col-span-1 flex flex-col justify-center bg-orange-lighter text-purple min-h-screen px-10 py-10">
+        <div className="col-span-1 flex flex-col justify-center bg-orange text-purple min-h-screen px-10 py-10">
           <p className="mt-4 text-3xl font-bold">The Pawfect Match</p>
           <p className="Lexend mt-4">
             The Pawfect Match is here to help dog lovers like you search through
@@ -39,7 +39,7 @@ function App() {
             </>
           )}
         </div>
-        <div className="col-span-2 bg-blue-100 min-h-screen px-10 py-10">
+        <div className="col-span-2 bg-purple min-h-screen px-10 py-10">
           <Routes>
             {currUser ? (
               <>
