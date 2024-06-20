6bf4eb8a0418223bf5081636b43bf0856c83cfa0
Elise Ngo <elisetvy@gmail.com>
2024-06-20 14:11:44 -0700

    Restyle form so button is consistent with other buttons

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -40,8 +40,8 @@ function Auth({ setCurrUser }: AuthProps) {
 
   return (
     <div className="h-full flex flex-col justify-center items-center text-center">
-      <form className="Lexend text-white w-1/2 bg-purple px-10 py-10 rounded-xl mx-10 flex flex-col items-center min-w-1/3 ">
-      <h1 className="text-2xl font-bold pb-6">Let's Get Started</h1>
+      <form className="Lexend text-purple w-1/2 bg-slate-100 drop-shadow-md px-10 py-10 rounded-xl mx-10 flex flex-col items-center min-w-1/3 ">
+        <h1 className="text-2xl font-bold pb-6">Let's Get Started</h1>
         <div className="grid grid-cols-6 gap-2 w-full pb-4">
           <label htmlFor="name" className="">
             Name:
@@ -49,7 +49,7 @@ function Auth({ setCurrUser }: AuthProps) {
           <input
             id="name"
             name="name"
-            className="col-span-5 bg-slate-100 px-2 py-1 w-full rounded-xl text-purple"
+            className="col-span-5 bg-purple text-white px-2 py-1 w-full rounded-xl"
             required
             value={userData.name}
             onChange={handleChange}
@@ -63,7 +63,7 @@ function Auth({ setCurrUser }: AuthProps) {
           <input
             id="email"
             name="email"
-            className="col-span-5 bg-slate-100 px-2 py-1 w-full rounded-xl text-purple"
+            className="col-span-5 bg-purple text-white px-2 py-1 w-full rounded-xl"
             required
             value={userData.email}
             onChange={handleChange}
@@ -73,7 +73,7 @@ function Auth({ setCurrUser }: AuthProps) {
         {error && <p className="mt-6 text-white">{error}</p>}
         <button
           onClick={handleSubmit}
-          className="bg-orange rounded-xl px-6 py-2 w-fit mt-6 text-white hover:opacity-85"
+          className="bg-purple rounded-xl px-6 py-2 w-fit mt-6 text-white hover:bg-[#300d38]"
         >
           Log In
         </button>
