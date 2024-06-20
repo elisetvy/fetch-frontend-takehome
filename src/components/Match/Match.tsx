68b3841f8b680ab125ddd7a787a7d2a5ae881deb
Elise Ngo <elisetvy@gmail.com>
2024-06-19 20:25:32 -0700

    Remove text blurb and conditional rendering

--- src/components/Match/Match.tsx
+++ src/components/Match/Match.tsx
@@ -8,13 +8,11 @@ import { Link } from "react-router-dom";
 function Match() {
   const [match, setMatch] = useState(null);
   const [matched, setMatched] = useState(false);
-  const favorites = sessionStorage.getItem("favorites");
 
   async function handleClick(e) {
     e.preventDefault();
 
     try {
-      const favoritesArray = JSON.parse(favorites);
       const match = await Api.getMatch(favoritesArray);
       const matchedDog = await Api.fetchDogs([match.match]);
       setMatch(matchedDog[0]);
@@ -26,11 +24,6 @@ function Match() {
 
   return (
     <div className="flex flex-col items-center">
-      {favorites === null && (
-        <h1 className="Lexend mt-4 text-red-500">
-          Start favoriting some dogs to find a pawfect match!
-        </h1>
-      )}
       {matched === true && (
         <div className="flex flex-col items-center">
           <h1 className="Lexend my-4 text-red-500">
@@ -39,13 +32,11 @@ function Match() {
           <Dog dog={match} />
         </div>
       )}
-      {favorites !== null && (
-        <Link to="/favorites">
-          <button className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white">
-            Find Your Pawfect Match!
-          </button>
-        </Link>
-      )}
+      <Link to="/favorites">
+        <button className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white">
+          Find Your Pawfect Match!
+        </button>
+      </Link>
     </div>
   );
 }
