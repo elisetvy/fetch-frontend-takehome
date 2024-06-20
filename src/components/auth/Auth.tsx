5b8c67502363ae0dcb0118c601e12ad78b7d0c5a
Elise Ngo <elisetvy@gmail.com>
2024-06-20 12:35:28 -0700

    Add fetch colors

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -40,7 +40,7 @@ function Auth({ setCurrUser }: AuthProps) {
 
   return (
     <div className="h-full flex flex-col justify-center items-center text-center">
-      <form className="Lexend w-1/2 bg-slate-100 px-10 py-10 rounded-xl mx-10 flex flex-col items-center min-w-1/3">
+      <form className="Lexend text-white w-1/2 bg-purple px-10 py-10 rounded-xl mx-10 flex flex-col items-center min-w-1/3 ">
       <h1 className="text-2xl font-bold pb-6">Let's Get Started</h1>
         <div className="grid grid-cols-6 gap-2 w-full pb-4">
           <label htmlFor="name" className="">
@@ -49,7 +49,7 @@ function Auth({ setCurrUser }: AuthProps) {
           <input
             id="name"
             name="name"
-            className="col-span-5 bg-blue-100 px-2 py-1 w-full rounded-xl"
+            className="col-span-5 bg-slate-100 px-2 py-1 w-full rounded-xl text-purple"
             required
             value={userData.name}
             onChange={handleChange}
@@ -63,17 +63,17 @@ function Auth({ setCurrUser }: AuthProps) {
           <input
             id="email"
             name="email"
-            className="col-span-5 bg-blue-100 px-2 py-1 w-full rounded-xl"
+            className="col-span-5 bg-slate-100 px-2 py-1 w-full rounded-xl text-purple"
             required
             value={userData.email}
             onChange={handleChange}
             type="email"
           ></input>
         </div>
-        {error && <p className="mt-4 text-red-500">{error}</p>}
+        {error && <p className="mt-6 text-white">{error}</p>}
         <button
           onClick={handleSubmit}
-          className="bg-blue-600 rounded-xl px-6 py-2 w-fit mt-6 text-white hover:opacity-85"
+          className="bg-orange rounded-xl px-6 py-2 w-fit mt-6 text-white hover:opacity-85"
         >
           Log In
         </button>
