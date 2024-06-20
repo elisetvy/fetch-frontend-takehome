1afe13784b6a347f17fee6b6986487e338e2071d
Elise Ngo <elisetvy@gmail.com>
2024-06-19 21:11:58 -0700

    Add see all dogs button

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,11 @@ function App() {
           {currUser && (
             <>
               <div className="mt-4 flex flex-col items-start">
+                <Link to="/">
+                  <button className="Lexend mt-4 bg-blue-500 px-4 py-2 rounded-xl hover:bg-blue-600 text-white">
+                    See All Dogs
+                  </button>
+                </Link>{" "}
                 <Match />
                 <LogOut setCurrUser={setCurrUser} />
               </div>
