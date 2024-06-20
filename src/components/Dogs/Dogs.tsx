53c58a3a78ca4e85f21f0acfe069e54e0e8617c7
Elise Ngo <elisetvy@gmail.com>
2024-06-20 15:00:38 -0700

    Remove unused imports

--- src/components/Dogs/Dogs.tsx
+++ src/components/Dogs/Dogs.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { useState, useEffect } from "react";
 
 import Search from "../Search/Search";
 import Dog from "../Dog/Dog";
