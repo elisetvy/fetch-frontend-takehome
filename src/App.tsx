17ad64ca4df053148164d2cbcf2186610117c4ac
Elise Ngo <elisetvy@gmail.com>
2024-06-20 14:29:43 -0700

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ function App() {
           <p className="mt-4 text-3xl font-bold">The Pawfect Match</p>
           <p className="Lexend mt-4">
             The Pawfect Match is here to help dog lovers like you search through
-            a database of shelter dogs, making it easy to find your furry new
-            best friend!
+            a database of shelter dogs, making it easy to find a lucky dog a new
+            home!
           </p>
           {currUser && (
             <>
@@ -67,42 +67,6 @@ function App() {
           </Routes>
         </div>
       </div>
-      {/* <div className="flex flex-col items-center mx-10 my-10">
-        {currUser && <Nav setCurrUser={setCurrUser} />}
-        <Link to="/" className="mt-4 text-3xl font-bold">
-          The Pawfect Match
-        </Link>
-        <p className="Lexend mt-4 min-w-2/4 text-center">
-          The Pawfect Match is here to help dog lovers like you search through a
-          database of shelter dogs, making it easy to find your furry new best
-          friend!
-        </p>
-        <Routes>
-          {currUser ? (
-            <>
-              <Route
-                path="/"
-                element={<Dogs setFavorites={setFavorites} />}
-              ></Route>
-              <Route
-                path="/favorites"
-                element={
-                  <Favorites
-                    favorites={favorites}
-                    setFavorites={setFavorites}
-                  />
-                }
-              />
-            </>
-          ) : (
-            <Route
-              path="/"
-              element={<Auth setCurrUser={setCurrUser} />}
-            ></Route>
-          )}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div> */}
     </BrowserRouter>
   );
 }
